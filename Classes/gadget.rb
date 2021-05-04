1bd69bb9e3f15124af7d0ea4eed5c55207017ffb
Paul Dennis <paul.dennis@hotmail.com>
2021-05-04 10:43:51 -0500

    Showing instance variables and initialize method

--- Classes/gadget.rb
+++ Classes/gadget.rb
@@ -1,3 +1,30 @@
 # use the class keyword and use CamelCase for class names, and follow it by end
+
+# instance variables define the object's properties and represent
+#   the object's state
+# instance variables start with the @ symbol called a sigil.  This denotes
+#   the variable's scope, and without this sigil the variable would be
+#   interpreted as a local variable to the method it is used in
+# We initialize the instance variables with the initialize method.  This
+#     method will be called every time the .new method is called
+
+
 class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+
+  def to_s
+    "#{@username}  |  #{@password}  |  #{@production_number}"
+  end
 end
+
+phone = Gadget.new
+laptop = Gadget.new
+
+p phone.to_s
+p laptop.to_s
+
+p phone.instance_variables
