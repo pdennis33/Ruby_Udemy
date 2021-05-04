9b362598f8512f274729c5b28e01a6ada08051a7
Paul Dennis <paul.dennis@hotmail.com>
2021-05-04 11:06:00 -0500

    Overriding methods

--- Classes/gadget.rb
+++ Classes/gadget.rb
@@ -1,7 +1,6 @@
 # use the class keyword and use CamelCase for class names, and follow it by end
 
-# instance methods are methods that belong exclusively on the object
-
+# Method overriding
 
 class Gadget
   def initialize
@@ -10,17 +9,11 @@ class Gadget
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
 
-  # instance method
-  def info
+  # instance method, overriding the to_s method from the Object class
+  def to_s
     "Gadget #{@production_number} has the username #{@username}"
   end
 end
 
-
 phone = Gadget.new
-laptop = Gadget.new
-
-p phone.info
-p laptop.info
-
-p phone.methods - Object.methods
+p phone.to_s
