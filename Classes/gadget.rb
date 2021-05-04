326439d0318f923b6607952e575e1996d70ee70a
Paul Dennis <paul.dennis@hotmail.com>
2021-05-04 12:14:07 -0500

    Using getter and setter shortcuts with attr_

--- Classes/gadget.rb
+++ Classes/gadget.rb
@@ -1,9 +1,16 @@
 # use the class keyword and use CamelCase for class names, and follow it by end
 
-# Setter methods allow you to expose properties to outside of the class
-#    for editing them
+# Better way to write Getters and Setters, called Accessor methods
+#  One way is to use attr_accessor, attr_reader, or attr_writer
 
 class Gadget
+
+  attr_accessor :username # read and write access
+  attr_reader :production_number # read only
+  attr_writer :password # write only
+  # we can also set multiple variables on one line,
+  #    such as attr_writer :password, :username
+
   def initialize
     @username = "User #{rand(1..100)}"
     @password = "topsecret"
@@ -17,34 +24,13 @@ class Gadget
     val
   end
 
-  # Getters and Setters
-  def username
-    @username
-  end
-
-  # Setter (best practice is to close the method name with an =)
-  def username=(new_username)
-    @username = new_username
-  end
-
-  def production_number
-    @production_number
-  end
-
-  # Setter (best practice is to close the method name with an =)
-  def password=(password)
-    @password = password
-  end
-
 end
 
 phone = Gadget.new
 p phone.production_number
 
 p phone.username
-phone.username=("pdennis")
+phone.username = "pdennis"
 p phone.username
-
-phone.password=("very_secure")
 # we can also use the regular syntax
 p phone.password = "really really secure"
