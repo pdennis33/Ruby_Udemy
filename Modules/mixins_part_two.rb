d0cc6d24f57f6bcfb481fe8fd23f54bdc642593d
Paul Dennis <paul.dennis@hotmail.com>
2021-05-05 10:43:42 -0500

    Showing ancestors and inheritance in-depth

--- Modules/mixins_part_two.rb
+++ Modules/mixins_part_two.rb
@@ -6,14 +6,25 @@ end
 
 class Bookstore
   include Purchaseable
+
+  def purchase(item)
+    "You bought a copy of #{item} at the bookstore!"
+  end
 end
 
 class Supermarket
   include Purchaseable
+
+  def purchase(item)
+    "Thanks for visiting the supermarket and buying #{item}"
+  end
 end
 
 # CornerMart inherits from Supermarket, so it is also Purchaseable
 class CornerMart < Supermarket
+  def purchase(item)
+    "Thanks for stopping and buying #{item}. Come again soon!"
+  end
 end
 
 barnes_and_noble = Bookstore.new
@@ -24,3 +35,9 @@ p whole_foods.purchase("Apples")
 
 quickstop = CornerMart.new
 p quickstop.purchase("Bread")
+
+p Bookstore.ancestors
+bn = Bookstore.new
+p bn.purchase("1984")
+p CornerMart.ancestors
+p Supermarket.ancestors
