46fb79b5b28b7207255ff0fdb940df0ecef4d494
Paul Dennis <paul.dennis@hotmail.com>
2021-05-07 12:39:35 -0500

    Using hashes with default values and the merge method

--- Classes/candidate.rb
+++ Classes/candidate.rb
@@ -1,12 +1,16 @@
 class Candidate
   attr_accessor :name, :age, :occupation, :hobby, :birthplace
 
-  def initialize(name, details)
+  def initialize(name, details = {})
+    defaults = {age: 35, occupation: "Candidate",
+        hobby: "Running for office", birthplace: "USA"}
+    defaults.merge!(details)
+
     @name = name
-    @age = details[:age]
-    @occupation = details[:occupation]
-    @hobby = details[:hobby]
-    @birthplace = details[:birthplace]
+    @age = defaults[:age]
+    @occupation = defaults[:occupation]
+    @hobby = defaults[:hobby]
+    @birthplace = defaults[:birthplace]
   end
 
 end
@@ -20,8 +24,10 @@ p senator.occupation
 
 puts
 
-info = {age: 48, hobby: "Football", birthplace: "Illinois"}
+info = {hobby: "Horror movies", occupation: "Popcorn vendor"}
 other_guy = Candidate.new("Mr Doe", info)
 p other_guy.name
 p other_guy.hobby
 p other_guy.occupation
+p other_guy.age
+p other_guy.birthplace
