13969f271488f4b539393482af137593cc825b50
Paul Dennis <paul.dennis@hotmail.com>
2021-05-07 21:48:08 -0500

    Using rdoc with comments to add descriptions in documentation

--- rdoc/album.rb
+++ rdoc/album.rb
@@ -1,15 +1,21 @@
+# An Album class that stores an array of songs
 class Album
   include Enumerable
+
+  # An array object of songs. Each song is a String
   attr_reader :songs
 
+  # Creates a new Album instance with an empty songs array
   def initialize
     @songs = []
   end
 
+  # Adds a song to the object's songs array
   def add_song(song)
     songs << song
   end
 
+  # Yield each in song in the Album to the block
   def each
     songs.each { |song| yield song }
   end
